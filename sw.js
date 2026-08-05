87146fa8c48b62b75be213be6a34ac0c7bca2689
Magnifico <magnificolv@users.noreply.github.com>
2026-08-05 14:28:46 +0300

    brand: official icon #5 + v3.7.1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * NEW versions install in background but do NOT activate until user taps «Atjaunināt».
  * No forced reloads mid-session.
  */
-const APP_VERSION = '3.7.0';
+const APP_VERSION = '3.7.1';
 const CACHE = 'darba-kalendars-v' + APP_VERSION;
 
 const PRECACHE = [
